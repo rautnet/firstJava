8c42b37883cf25fb1fc79f8e58e5466d1282e98f
rautnet <50319701+rautnet@users.noreply.github.com>
2020-04-07 19:05:05 +0530

    updating date

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, New World New Date New Change !!! "+new Date());
+    System.out.println("Hello, New World New Date 7 April 2020 New Change !!! ");
   }
 }
